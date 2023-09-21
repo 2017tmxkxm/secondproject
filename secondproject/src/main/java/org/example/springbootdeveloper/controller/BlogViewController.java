48045c35ba9f0e807dbdcd18c50a28413a8d7fc5
2017tmxkxm <2017tmxkxm@gmail.com>
2023-09-21 12:22:27 +0900

    블로그 글 view 코드 추가

--- secondproject/src/main/java/org/example/springbootdeveloper/controller/BlogViewController.java
+++ secondproject/src/main/java/org/example/springbootdeveloper/controller/BlogViewController.java
@@ -2,11 +2,14 @@ package org.example.springbootdeveloper.controller;
 
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
+import org.example.springbootdeveloper.domain.Article;
 import org.example.springbootdeveloper.dto.ArticleListViewResponse;
+import org.example.springbootdeveloper.dto.ArticleViewResponse;
 import org.example.springbootdeveloper.service.BlogService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import java.util.List;
 
@@ -26,4 +29,14 @@ public class BlogViewController {
 
         return "articleList";
     }
+
+    @GetMapping("/articles/{id}")
+    public String getArticle(@PathVariable Long id, Model model) {
+        Article article = blogService.findById(id);
+
+        model.addAttribute("article", new ArticleViewResponse(article));
+
+        return "article";
+    }
+
 }
--- secondproject/src/main/java/org/example/springbootdeveloper/domain/Article.java
+++ secondproject/src/main/java/org/example/springbootdeveloper/domain/Article.java
@@ -2,6 +2,10 @@ package org.example.springbootdeveloper.domain;
 
 import jakarta.persistence.*;
 import lombok.*;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+
+import java.time.LocalDateTime;
 
 @Entity
 @Getter
@@ -25,8 +29,19 @@ public class Article {
         this.content = content;
     }
 
+    // 엔티티가 생성될 때 생성 시간 저장
+    @CreatedDate
+    @Column(name = "created_at")
+    private LocalDateTime createdAt;
+
+    // 엔티티가 수정될 때 수정 시간 저장
+    @LastModifiedDate
+    @Column(name = "updated_at")
+    private LocalDateTime updatedAt;
+
     public void update(String title, String content) {
         this.title = title;
         this.content = content;
     }
+
 }

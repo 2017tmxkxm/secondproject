a4bf3f9a551f72d87de56b1d153ed98ab05a285a
2017tmxkxm <2017tmxkxm@gmail.com>
2023-09-24 18:41:14 +0900

    회원가입, 로그인 코드 추가

---
+++ secondproject/src/main/java/org/example/springbootdeveloper/controller/UserviewController.java
@@ -0,0 +1,20 @@
+package org.example.springbootdeveloper.controller;
+
+import org.hibernate.annotations.Comment;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class UserviewController {
+
+    @GetMapping("/login")
+    public String login() {
+        return "login";
+    }
+
+    @GetMapping("/signup")
+    public String signup() {
+        return "signup";
+    }
+
+}

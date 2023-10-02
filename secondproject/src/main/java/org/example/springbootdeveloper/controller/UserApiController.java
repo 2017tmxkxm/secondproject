f0cec77bfdcb48a7ee4aa204bc776c33eb0cbc5f
2017tmxkxm <2017tmxkxm@gmail.com>
2023-10-02 11:31:45 +0900

    토큰 생성 로직 추가

--- secondproject/src/main/java/org/example/springbootdeveloper/controller/UserApiController.java
+++ secondproject/src/main/java/org/example/springbootdeveloper/controller/UserApiController.java
@@ -3,6 +3,7 @@ package org.example.springbootdeveloper.controller;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.example.springbootdeveloper.dto.AddUserRequest;
 import org.example.springbootdeveloper.service.UserService;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -11,6 +12,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
+@Slf4j
 @RequiredArgsConstructor
 @Controller
 public class UserApiController {
---
+++ secondproject/src/main/java/org/example/springbootdeveloper/repository/RefreshTokenRepository.java
@@ -0,0 +1,12 @@
+package org.example.springbootdeveloper.repository;
+
+import org.example.springbootdeveloper.domain.RefreshToken;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import javax.swing.text.html.Option;
+import java.util.Optional;
+
+public interface RefreshTokenRepository extends JpaRepository<RefreshToken, Long> {
+    Optional<RefreshToken> findByUserId(Long userId);
+    Optional<RefreshToken> findByRefreshToken(String refreshToken);
+}
